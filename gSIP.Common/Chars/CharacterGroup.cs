f0422d97588ba4758ac89983303a311ef3bf78b0
Pavel Goleshchikhin <goleshchikhin@yandex.ru>
2018-01-01 21:48:37 +0700

    ГПВ 01.01.2018 - 01

--- gSIP.Common/Chars/CharacterGroup.cs
+++ gSIP.Common/Chars/CharacterGroup.cs
@@ -24,7 +24,7 @@ namespace gSIP.Common.Chars
         /// <summary>
         /// Массив диапазонов символов.
         /// </summary>
-        protected char[][] CharsRange;
+        protected char[,] CharsRanges;
 
         /// <summary>
         /// Показывает, относится ли указанный символ к разрешенным в рамках данного набора.
@@ -41,14 +41,15 @@ namespace gSIP.Common.Chars
         protected CharacterGroup(string name)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name), "Не задано имя набора символов.");
-            Chars = new char[0];
-            CharsRange = new char[0][];
+            Chars = null;
+            CharsRanges = null;
         }
 
         /// <summary>
         /// Добавление массива с набором символов.
         /// </summary>
         /// <param name="chars">Массив с набором символов.</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public void AddChars(char[] chars)
         {
             if (chars != null && chars.Length != 0)
@@ -74,11 +75,67 @@ namespace gSIP.Common.Chars
 
                 Chars = newCharsList.ToArray();
             }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(chars), "Массив с символами не инициализирован или пуст.");
+            }
         }
 
+        /// <summary>
+        /// Добавление диапазона символов.
+        /// </summary>
+        /// <param name="firstCharacter">Первый символ диапазона.</param>
+        /// <param name="lastCharacter">Последний символ диапазона.</param>
+        /// <exception cref="Exception"></exception>
         public void AddCharsRange(char firstCharacter, char lastCharacter)
         {
+            if (firstCharacter < lastCharacter)
+            {
+                if (CharsRanges != null)
+                {
+                    for (int i = 0; i <= CharsRanges.GetUpperBound(0); i++)
+                    {
+                        if ( firstCharacter >= CharsRanges[i, 0] 
+                            && firstCharacter <= CharsRanges[i, 1] 
+                            && lastCharacter > CharsRanges[i, 1] )
+                        {
+                            CharsRanges[i, 1] = lastCharacter;
+                            return;
+                        }
+                        else if ( lastCharacter >= CharsRanges[i, 0]
+                            && lastCharacter <= CharsRanges[i, 1]
+                            && firstCharacter < CharsRanges[i, 0] )
+                        {
+                            CharsRanges[i, 0] = firstCharacter;
+                            return;
+                        }
+                    }
 
+                    char[,] newCharsRanges = new char[CharsRanges.GetUpperBound(0) + 2, 2];
+
+                    try
+                    {
+                        Array.Copy(CharsRanges, 0, newCharsRanges, 0, CharsRanges.GetUpperBound(0) + 1);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Ошибка копирования массива CharsRanges в newCharsRanges.", ex);
+                    }
+                    
+                    newCharsRanges[newCharsRanges.GetUpperBound(0), 0] = firstCharacter;
+                    newCharsRanges[newCharsRanges.GetUpperBound(0), 1] = lastCharacter;
+
+                    CharsRanges = newCharsRanges;
+                }
+                else
+                {
+                    CharsRanges = new char[1, 2] { { firstCharacter, firstCharacter } };
+                }
+            }
+            else
+            {
+                throw new Exception("Диапазон символов задан неверно.");
+            }
         }
 
         /// <summary>
@@ -110,7 +167,55 @@ namespace gSIP.Common.Chars
         /// <returns>Строка, представляющая текущий объект.</returns>
         public override string ToString()
         {
-            return string.Format("character group \"{0}\"", Name);
+            return string.Format("набор символов \"{0}\"", Name);
+        }
+
+        /// <summary>
+        /// Возвращает строку с символами из массива.
+        /// </summary>
+        /// <returns>Строка, с символами из массива.</returns>
+        public string ToStringChars()
+        {
+            if (Chars != null && Chars.Length > 0)
+            {
+                StringBuilder sb = new StringBuilder(Chars.Length);
+
+                for (int i = 0; i < Chars.Length; i++)
+                {
+                    sb.Append(Chars[i]);
+                }
+
+                return sb.ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает строку с диапазонами символов.
+        /// </summary>
+        /// <returns>Строка, с диапазонами символов.</returns>
+        public string ToStringCharsRanges()
+        {
+            if (CharsRanges != null && CharsRanges.GetUpperBound(0) > 0)
+            {
+                StringBuilder sb = new StringBuilder(CharsRanges.GetUpperBound(0) * 4);
+
+                for (int i = 0; i < CharsRanges.GetUpperBound(0); i++)
+                {
+                    sb.AppendFormat("{0}-{1} ", 
+                        CharsRanges[i, 0].ToString(), 
+                        CharsRanges[i, 1].ToString());
+                }
+
+                return sb.ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
     }
 }
---
+++ gSIP.Common/Chars/CharacterGroupPositive.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gSIP.Common.Chars
+{
+    public class CharacterGroupPositive : CharacterGroup
+    {
+        /// <summary>
+        /// Конструктор класса CharacterGroupPositive.
+        /// </summary>
+        /// <param name="name">Наименование набора символов.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected CharacterGroupPositive(string name) : base(name)
+        {
+        }
+
+        /// <summary>
+        /// Показывает, относится ли указанный символ к разрешенным в рамках данного набора.
+        /// </summary>
+        /// <param name="ch">Проверяемый символ.</param>
+        /// <returns>Значение true, если символ разрешен; в противном случае — значение false.</returns>
+        public override bool IsCharAllowed(char ch)
+        {
+            bool result = false;
+
+            if (CharsRanges != null && CharsRanges.GetUpperBound(0) >= 0)
+            {
+                for (int i = 0; i <= CharsRanges.GetUpperBound(0); i++)
+                {
+                    if (ch >= CharsRanges[i, 0] && ch <= CharsRanges[i, 1])
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!result && Chars != null && Chars.Length > 0)
+            {
+                for (int i = 0; i < Chars.Length; i++)
+                {
+                    if (ch.Equals(Chars[i]))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
